739756ab03137e44249ef8574de397c7a9c21511
guoguo <2354687725@qq.com>
2020-04-04 16:17:14 +0800

    修复默认模板手机端分类页选择问题

--- app/index/controller/Rank.php
+++ app/index/controller/Rank.php
@@ -57,36 +57,4 @@ class Rank extends Base
         ]);
         return view($this->tpl);
     }
-
-    public function getRanks()
-    {
-        $books = array();
-        $op = input('op');
-        $date = input('date');
-        $time = date('Y-m-d', strtotime('-1 ' . $date));
-
-        if ($op == 'click') {
-            $books = $this->bookService->getHotBooks($this->prefix, $this->end_point, $time, 30);
-        } elseif ($op == 'new') {
-            $books = cache('newest_homepage');
-            if (!$books) {
-                $books = $this->bookService->getBooks($this->end_point, 'last_time', '1=1', 30);
-                cache('newest_homepage', $books, null, 'redis');
-            }
-        } elseif ($op == 'end') {
-            $books = cache('ends_homepage');
-            if (!$books) {
-                $books = $this->bookService->getBooks( $this->end_point, 'last_time', [['end', '=', '2']], 30);
-                cache('ends_homepage', $books, null, 'redis');
-            }
-        }
-
-        foreach ($books as &$book) {
-            $book['date'] = date('Y-m-d H:i:s', $book['last_time']);
-            Db::query('SELECT * FROM '.$this->prefix.
-                'chapter WHERE id = (SELECT MAX(id) FROM (SELECT id FROM xwx_chapter WHERE book_id=?) as a)',
-                [$book['id']])[0];
-        }
-        return json(['books' => $books, 'success' => 1]);
-    }
 }
