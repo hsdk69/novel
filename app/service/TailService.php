864838faf0fd38ab7a984bb79e48771635d73d9a
guoguo <2354687725@qq.com>
2021-03-30 18:25:03 +0800

    美化分页样式

--- app/service/TailService.php
+++ app/service/TailService.php
@@ -9,17 +9,31 @@ use think\facade\Db;
 
 class TailService
 {
-    public function getlist($num, $end_point, $order = 'id')
+    public function getlist($num, $order = 'id')
     {
-        $n = Tail::select()->count();
-        if ($num > $n) {
-            $num = $n;
-        }
-        $random_number_array = range(0, $n);
-        shuffle($random_number_array );
-        $random_number_array = array_slice($random_number_array ,0,$num);
-        $tails = Tail::with('book.cate')->where('id','in',$random_number_array)->select();
-
-        return $tails;
+//        $n = Tail::select()->count();
+//        if ($num > $n) {
+//            $num = $n;
+//        }
+//        $random_number_array = range(0, $n);
+//        shuffle($random_number_array );
+//        $random_number_array = array_slice($random_number_array ,0,$num);
+//        $tails = Tail::with('book.cate')->where('id','in',$random_number_array)->select();
+        $data = Tail::with('book.cate')->order('id', 'desc')
+            ->paginate([
+                'list_rows' => $num,
+                'query' => request()->param(),
+            ]);
+        $tails = $data->toArray();
+        return [
+            'tails' => $tails['data'],
+            'page' => [
+                'total' => $tails['total'],
+                'per_page' => $tails['per_page'],
+                'current_page' => $tails['current_page'],
+                'last_page' => $tails['last_page'],
+                'query' => request()->param()
+            ]
+        ];
     }
 }
