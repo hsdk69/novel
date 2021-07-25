12a6f27b8dc9c274083c2fd23f29ab9f774bd41c
guoguo <2354687725@qq.com>
2021-07-25 20:59:25 +0800

    修复tp文件缓存bug

--- vendor/topthink/framework/src/think/cache/driver/File.php
+++ vendor/topthink/framework/src/think/cache/driver/File.php
@@ -105,7 +105,8 @@ class File extends Driver
 
             if ($this->options['data_compress'] && function_exists('gzcompress')) {
                 //启用数据压缩
-                $content = gzuncompress($content);
+                $content = preg_replace_callback('#s:(\d+):"(.*?)";#s',function($match){return 's:'.strlen($match[2]).':"'.$match[2].'";';},$content);
+                $content = unserialize($content);
             }
 
             return is_string($content) ? ['content' => $content, 'expire' => $expire] : null;
